c19caab39286a60324b1acde852af36e6024f979
Armish-W <f24bscs013@kinnaird.edu.pk>
2026-08-04 08:18:11 +0000

    Variables and Datatypes

---
+++ 01-Basics/01-datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat js code as newer version
+let accountId=12
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+// alert(3+3) we are using nodejs not browser
+/*
+alert mdm
+ecma (tc39)
+standards to follow
+
+data types:
+number
+bigint
+string
+boolean
+null (standalone value) eg accountId=null
+undefined (type is also undefined)
+symbol (uniqueness)
+
+objects:
+// typeof gives null type as object
+*/
+
---
+++ 01-Basics/01-variables.js
@@ -0,0 +1,13 @@
+const accountId = 144565
+let accountEmail = "aw@gmail.com"
+var accountPassword = "hi123"
+let accountState;
+ accountCity = "Lahore"
+// accountId=2 // not allowed
+accountEmail = "aw.1@gmail.com"
+accountPassword = "hi1234"
+ accountCity = "Lailpur"
+ console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
+/* 
+prefer not to use var because of the scope issue
+*/
