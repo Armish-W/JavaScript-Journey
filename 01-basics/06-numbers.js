2530d2a458de34c6a00c72305b07c9e37c4437f1
Armish-W <f24bscs013@kinnaird.edu.pk>
2026-08-05 03:27:25 +0000

    numbers

---
+++ 01-basics/06-numbers.js
@@ -0,0 +1,7 @@
+const num= new Number(49)
+console.log(num);
+console.log(num.toString());
+console.log(num.toFixed(2));
+console.log(num.toString().length);
+const hundred= 1000000
+console.log(hundred.toLocaleString());
