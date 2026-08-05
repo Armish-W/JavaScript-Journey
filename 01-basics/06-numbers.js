9cd7ff7682a85fdd3b3161a8f6ad503561c2eb44
Armish-W <f24bscs013@kinnaird.edu.pk>
2026-08-05 04:54:26 +0000

    Math library

--- 01-basics/06-numbers.js
+++ 01-basics/06-numbers.js
@@ -5,3 +5,20 @@ console.log(num.toFixed(2));
 console.log(num.toString().length);
 const hundred= 1000000
 console.log(hundred.toLocaleString());
+// maths
+console.log(Math);
+console.log(Math.abs(-112));
+console.log(Math.round(-112.5));
+console.log(Math.ceil(112.1));
+console.log(Math.floor(112.99));
+console.log(Math.ceil(112.001));
+console.log(Math.pow(2,3));
+console.log(Math.min(-1,1,2));
+console.log(Math.max(-1,1,2));
+console.log(Math.random()); // value between 0 and 1
+console.log((Math.random()*10)+1); // to prevent from getting value in 0s
+console.log(Math.floor(Math.random()*10)+1);
+
+const max= 20
+const min = 10
+console.log(Math.floor(Math.random()*(max-min+1)+min));
